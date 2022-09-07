a784b173389e3dba8f60bc1399b3ce856bdf7a5a
megmgleason <marymargaretgleason@gmail.com>
2022-09-07 16:36:47 -0400

    Initial Commit

---
+++ SwiftUIButton/SwiftUIButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonApp.swift
+//  SwiftUIButton
+//
+//  Created by MEG G on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
