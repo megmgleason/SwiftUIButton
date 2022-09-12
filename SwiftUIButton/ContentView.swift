9873a21515063148f298aad9735309d0a7c6a3e2
megmgleason <marymargaretgleason@gmail.com>
2022-09-12 10:52:49 -0400

    Gradient Button Style - no green gradient

--- SwiftUIButton/ContentView.swift
+++ SwiftUIButton/ContentView.swift
@@ -9,9 +9,94 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, worlds!")
-            .padding()
+        //        Button(action: {
+        //            // What to perform
+        //            print("Hello World tapped!")
+        //        }) {
+        //            // How the button looks like
+        //            Text("Hello World")
+        //                .fontWeight(.bold)
+        //                .font(.title)
+        //                .padding()
+        //                .background(Color.purple)
+        //                .cornerRadius(40)
+        //                .foregroundColor(.white)
+        //                .padding(10)
+        ////                .border(Color.purple, width: 5)
+        ////            overlay(
+        ////                RoundedRectangle(cornerRadius: 40)
+        ////                    .stroke(Color.purple, lineWidth: 5)
+        ////            )
+        //        }
+        
+        //        Button(action: {
+        //            print("Delete tapped!")
+        //        }) {
+        //            HStack {
+        //                Image(systemName: "trash")
+        //                    .font(.title)
+        //                Text("Delete")
+        //                    .fontWeight(.semibold)
+        //                    .font(.title)
+        //            }
+        //            .padding()
+        //            .foregroundColor(.white)
+        //            .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+        //            .cornerRadius(40)
+        //        }
+       VStack{
+            Button(action: {
+                print("Share tapped!")
+            }) {
+                HStack {
+                    Image(systemName: "square.and.arrow.up")
+                        .font(.title)
+                    Text("Share")
+                        .fontWeight(.semibold)
+                        .font(.title)
+                }
+            }
+            .buttonStyle(GradientBackgroundStyle())
+            
+            Button(action: {
+                print("Edit tapped!")
+            }) {
+                HStack {
+                    Image(systemName: "square.and.pencil")
+                        .font(.title)
+                    Text("Edit")
+                        .fontWeight(.semibold)
+                        .font(.title)
+                }
+            }
+            .buttonStyle(GradientBackgroundStyle())
+            
+            Button(action: {
+                print("Delete tapped!")
+            }) {
+                HStack {
+                    Image(systemName: "trash")
+                        .font(.title)
+                    Text("Delete")
+                        .fontWeight(.semibold)
+                        .font(.title)
+                }
+            }
+            .buttonStyle(GradientBackgroundStyle())
+        }
     }
+}
+
+struct GradientBackgroundStyle: ButtonStyle {
+    
+    func makeBody(configuration: Self.Configuration) -> some View { configuration.label
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            //.foregroundColor(.white)
+            .background(LinearGradient(gradient: Gradient(colors: [Color("DarkGreen"),Color("LightGreen")]), startPoint: .leading, endPoint: .trailing)) .cornerRadius(40)
+            .padding(.horizontal, 20)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
